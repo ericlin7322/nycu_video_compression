294bb88b515c36b6b1d3ce6271d892d24b94562b
Eric Lin <eric20706@gmail.com>
2023-10-19 03:30:29 +0800

    add hw2

---
+++ hw2/cv.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread("lena.png", cv2.IMREAD_GRAYSCALE)
+
+dct_coeffs = cv2.dct(np.float32(image))
+reconstructed_image = cv2.idct(dct_coeffs)
+
+cv2.imwrite("lena_cv2_dct.png", dct_coeffs)
+cv2.imwrite("lena_cv2_idct.png", reconstructed_image)
